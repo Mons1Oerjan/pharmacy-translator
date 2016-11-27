1442d284d798806b4b3c367ffbd1b9d9c2962a53
Mons1Oerjan <mons@dal.ca>
2016-11-27 12:03:36 -0400

    updated controller

--- src/main/java/com/dal/pharmacy_translator/controllers/GeneralController.java
+++ src/main/java/com/dal/pharmacy_translator/controllers/GeneralController.java
@@ -12,8 +12,11 @@ import com.dal.pharmacy_translator.dao.LanguageDAO;
 import com.dal.pharmacy_translator.dao.MedicineDAO;
 import com.dal.pharmacy_translator.dao.PatientDAO;
 import com.dal.pharmacy_translator.dao.TranslatedNotesDAO;
+import com.dal.pharmacy_translator.model.Code;
 import com.dal.pharmacy_translator.model.Language;
+import com.dal.pharmacy_translator.model.Medicine;
 import com.dal.pharmacy_translator.model.Patient;
+import com.dal.pharmacy_translator.model.TranslatedNotes;
 
 @Controller
 public class GeneralController {
@@ -38,11 +41,11 @@ public class GeneralController {
 			@RequestParam(value = "inputcode", required = false, defaultValue = "") String inputcode,
 			@RequestParam(value = "language", required = false, defaultValue = "") String language){
 		
-		ModelAndView mv = new ModelAndView("pharmacytranslator");
+		ModelAndView mv = new ModelAndView("helloworld");
 		
 		List<Patient> patients = patientDao.getList();
 		Patient p1 = new Patient(1, "Mons", "Norwegian", "1650 Walnut St", "10Nov1992", "9029892147", "mons@dal.ca", "Tor", "90047399");
-		Patient p2 = new Patient(1, "name", "language", "address", "birthdate", "phone#", "email", "emergency contact", "emergency phone#");
+		Patient p2 = new Patient(2, "name", "language", "address", "birthdate", "phone#", "email", "emergency contact", "emergency phone#");
 		patients.add(p1);
 		patients.add(p2);
 		
@@ -58,9 +61,23 @@ public class GeneralController {
 		languages.add(l4);
 		languages.add(l5);
 		
+		List<Medicine> medicines = medicineDao.getList();
+		Medicine m1 = new Medicine(1, "Puffer", 1); //id (unique), name, foreign key to patient
+		
+		List<TranslatedNotes> notes = translatednotesDao.getList();
+		TranslatedNotes tn1 = new TranslatedNotes(1, "Toma una tableta al día", 1, 1); //id, translation, foreign key language, foreign key code
+		TranslatedNotes tn2 = new TranslatedNotes(2, "تناول قرص واحد يوميا", 4, 1); //id, translation, foreign key language, foreign key code
+		
+		List<Code> codes = codeDao.getList();
+		Code c1 = new Code(1, "T1T1D", "Take 1 tablet daily", 1); //id (unique), code, definition, foreign key to medicine
+		
+		
 		//add to ModelAndView:
 		mv.addObject("patients", patients);
 		mv.addObject("languages", languages);
+		
+		
+		
 		
 		if(!inputcode.equals("")) {
 			mv.addObject("note","Note");
